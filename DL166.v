5a4b43ecaf43b51a3e3514e8bc7e618a06894a5c
michihiro imaoka <imaoca@gmail.com>
2020-09-13 16:18:42 +0900

    Update DL166.v
    fix carry flag(c_flag) operation

--- DL166.v
+++ DL166.v
@@ -47,31 +47,28 @@ module cpu(reset,clk,btn,led);
 	always @(posedge clk) begin
 		regs[5]=btn;
 		case(op0)
-//			4'b0000:begin a=a+im;c_flag=a[4];end	//ADD A,im
 			2'b00: regs[ddd]=regs[sss];
 			2'b01: 
-				case(op1)
-					3'b000:begin regs[0]=regs[0]+regs[sss];c_flag=regs[4][0];end//ADD take care c_flag
-					3'b001:regs[0]=regs[0]|regs[sss];
-					3'b010:regs[0]=regs[0]&regs[sss];
-					3'b011:regs[0]=regs[0]^regs[sss];
-					3'b100:begin regs[sss]=regs[sss]+1;c_flag=regs[4][sss]; end //INC take care c_flag
-					3'b101:regs[sss]=!regs[sss];
-					3'b110:begin c_flag=regs[0][sss];regs[sss]=regs[sss]>>1;end	//RSHIFT take care c_flag
-					3'b111:begin c_flag=regs[3][sss];regs[sss]=regs[sss]<<1;end	//LSHIFT take care c_flag
-				endcase
+			case(op1)
+			 3'b000:begin regs[0]=regs[0]+regs[sss];c_flag=regs[0][4];regs[0][4]=0;end//ADD take care c_flag
+			 3'b001:regs[0]=regs[0]|regs[sss];
+			 3'b010:regs[0]=regs[0]&regs[sss];
+			 3'b011:regs[0]=regs[0]^regs[sss];
+			 3'b100:begin regs[sss]=regs[sss]+1;c_flag=regs[sss][4];regs[sss][4]=0;end //INC take care c_flag
+			 3'b101:regs[sss]=!regs[sss];
+			 3'b110:begin c_flag=regs[sss][0];regs[sss]=regs[sss]>>1;end	//RSHIFT take care c_flag
+			 3'b111:begin c_flag=regs[sss][3];regs[sss]=regs[sss]<<1;end	//LSHIFT take care c_flag
+			endcase
 			2'b10:
-				case(op2)
-					2'b00:regs[7]= (c_flag)?regs[7]+1:imd;			//JNC
-					2'b01:regs[7]=imd;					//JMP
-					2'b10:regs[0]=imd;					
-				endcase
+			case(op2)
+			 2'b00:regs[7]= (c_flag)?regs[7]+1:imd;			//JNC
+			 2'b01:regs[7]=imd;					//JMP
+			 2'b10:regs[0]=imd;					//SET
+			endcase
 		endcase
 		
 		if(dout[7:4]!=4'b1000 && dout[7:4]!=4'b1001) regs[7]=regs[7]+1;
 		if(reset==0) regs[7] = 0;
-
-//	regs[7]=regs[7]+1;
 	end
 endmodule 
 
