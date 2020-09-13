5a3b744d4c52e6671cd9c7b7276de99b7a2adecd
michihiro imaoka <imaoca@gmail.com>
2020-09-14 06:37:11 +0900

    Update testbench3.v
    add reset operation

--- testbench3.v
+++ testbench3.v
@@ -1,11 +1,18 @@
 module testbench3();
 	reg clk;
-	reg reset=1;
+	reg reset = 1;
 	wire[3:0] btn;
 	wire[3:0]led;
 	cpu cpu0(reset,clk,btn,led);
-	always // no sensitivity list, so it always executes
-		begin
+
+	initial begin
+		reset = 0; #1;
+		clk = 1; #1; clk = 0; #1; // 10ns period
+		clk = 1; #1; clk = 0; #1; // 10ns period
+		reset = 1;	
+	end
+
+	always 	begin
 		clk = 1; #1; clk = 0; #1; // 10ns period
 	end
 endmodule
