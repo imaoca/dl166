080be03fb68c86919c9afe57bebcfdbdd1f79c9a
michihiro imaoka <imaoca@gmail.com>
2020-09-14 09:10:43 +0900

    Update DL166.v
    reduce source code less than before

--- DL166.v
+++ DL166.v
@@ -4,32 +4,26 @@ module cpu(input reset,input clk,input[3:0] btn,output[3:0]led);
 	reg [4:0]regs[7:0];
 	assign led= regs[6];
 	rom memory(dout, regs[7]);
-	wire[2:0] ddd,sss;
-	wire[3:0] imd;
-	assign ddd[2:0]=dout[5:3];//[2:0]
-	assign sss[2:0]=dout[2:0];//[2:0]
-	assign imd[3:0]=dout[3:0];//[3:0]
-
 	always @(posedge clk)
 	  if(reset==0) {regs[0],regs[1],regs[2],regs[3],regs[4],regs[7]}=0;
 	  else begin
 		regs[5]=btn;
 		case(dout[7:6])
-		2'b00: regs[ddd]=regs[sss];
+		2'b00: regs[dout[5:3]]=regs[dout[2:0]];
 		2'b01: case(dout[5:3])
-		  3'b000:begin regs[0]=regs[0]+regs[sss];c_flag=regs[0][4];regs[0][4]=0;end  //ADD take care c_flag
-		  3'b001:regs[0]=regs[0]|regs[sss];
-		  3'b010:regs[0]=regs[0]&regs[sss];
-		  3'b011:regs[0]=regs[0]^regs[sss];
-		  3'b100:begin regs[sss]=regs[sss]+1;c_flag=regs[sss][4];regs[sss][4]=0;end //INC take care c_flag
-		  3'b101:regs[sss]=!regs[sss];
-		  3'b110:begin c_flag=regs[sss][0];regs[sss]=regs[sss]>>1;end		     //RSHIFT take care c_flag
-		  3'b111:begin c_flag=regs[sss][3];regs[sss]=regs[sss]<<1;end		     //LSHIFT take care c_flag
+		  3'b000:begin regs[0]=regs[0]+regs[dout[2:0]];c_flag=regs[0][4];regs[0][4]=0;end  //ADD care c_flag
+		  3'b001:regs[0]=regs[0]|regs[dout[2:0]];
+		  3'b010:regs[0]=regs[0]&regs[dout[2:0]];
+		  3'b011:regs[0]=regs[0]^regs[dout[2:0]];
+		  3'b100:begin regs[dout[2:0]]=regs[dout[2:0]]+1;c_flag=regs[dout[2:0]][4];regs[dout[2:0]][4]=0;end //INC care c_flag
+		  3'b101:regs[dout[2:0]]=!regs[dout[2:0]];
+		  3'b110:begin c_flag=regs[dout[2:0]][0];regs[dout[2:0]]=regs[dout[2:0]]>>1;end		     //RSHIFT care c_flag
+		  3'b111:begin c_flag=regs[dout[2:0]][3];regs[dout[2:0]]=regs[dout[2:0]]<<1;end		     //LSHIFT care c_flag
 		 endcase
 		2'b10: case(dout[5:4])
-		  2'b00:regs[7]= (c_flag)?regs[7]+1:imd;		//JNC
-		  2'b01:regs[7]=imd;					//JMP
-		  2'b10:regs[0]=imd;					//SET
+		  2'b00:regs[7]= (c_flag)?regs[7]+1:dout[3:0];		//JNC
+		  2'b01:regs[7]=dout[3:0];				//JMP
+		  2'b10:regs[0]=dout[3:0];				//SET
 		 endcase
 		endcase		
 		if(dout[7:4]!=4'b1000 && dout[7:4]!=4'b1001) regs[7]=regs[7]+1;
