0b08c8dab3427bb2c699f86c9789db7985b8b21b
michihiro imaoka <imaoca@gmail.com>
2020-09-18 08:38:46 +0900

    Update testbench3.v
    modify to access external memory.

--- testbench3.v
+++ testbench3.v
@@ -3,7 +3,13 @@ module testbench3();
 	reg reset = 1;
 	wire[3:0] btn;
 	wire[3:0]led;
-	cpu cpu0(reset,clk,btn,led);
+	wire  [7:0] w_data;
+    	wire  [7:0] r_data;
+	wire  [3:0] adr;
+//	cpu cpu0(reset,clk,btn,led);
+	 ram ram1(clk, 0, {4'b0000,adr}, r_data, adr, w_data);  
+	 cpu cpu1(reset,clk,btn,led,adr,r_data);
+
 
 	initial begin
 		reset = 0; #1;
@@ -16,5 +22,3 @@ module testbench3();
 		clk = 1; #1; clk = 0; #1; // 10ns period
 	end
 endmodule
-
-
