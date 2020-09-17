f4ef41f7d79fd9b597e9a133855bc46546949a65
michihiro imaoka <imaoca@gmail.com>
2020-09-17 09:06:07 +0900

    Update rom.v
    adding some code to check flag operation.

--- rom.v
+++ rom.v
@@ -3,10 +3,10 @@ module rom(dout, addr);
 	input [3:0] addr;
 	reg[7:0] mem[15:0];
 	initial begin
-//		`include "asm.txt"
 		mem[ 4'd0] <= 8'b01100_000;// inc R0
 		mem[ 4'd1] <= 8'b1000_0000;// jnc 0
-		mem[ 4'd2] <= 8'b1001_0010;// jmp 2
+		mem[ 4'd2] <= 8'b1010_0000;// SET 0
+		mem[ 4'd3] <= 8'b1001_0000;// jmp 0
 	end
 	assign dout = mem[addr];
 endmodule
