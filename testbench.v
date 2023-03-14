b7c729e17724659867be0fbd30017b71f05a34f4
michihiro imaoka <imaoca@gmail.com>
2023-03-14 16:17:03 +0900

    Add files via upload

---
+++ testbench.v
@@ -0,0 +1,22 @@
+module testbench();
+	reg clk;
+	reg reset = 1;
+	wire[3:0] btn;
+	wire[3:0]led;
+	wire  [7:0] w_data;
+    	wire  [7:0] r_data;
+	wire  [3:0] adr;
+	 ram ram1(clk, 0, {4'b0000,adr}, r_data, adr, w_data);  
+	 cpu cpu1(reset,clk,btn,led,adr,r_data);
+ 	 initial begin
+		reset = 0; #1;
+		clk = 1; #1; clk = 0; #1; // 10ns period
+		clk = 1; #1; clk = 0; #1; // 10ns period
+		reset = 1;	
+	end
+	always 	begin
+		clk = 1; #1; clk = 0; #1; // 10ns period
+	end
+endmodule
+
+
